6379803ab220fc17fa8aa892e0def765d9a4ebc9
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-06-11 17:44:08 +0500

    Update MemeEngine.py

--- Engine/MemeEngine.py
+++ Engine/MemeEngine.py
@@ -17,24 +17,21 @@ class MemeEngine:
 		self.counter = 10
 
 	def make_meme(self, image, body, author, width=500):
+		font = ImageFont.truetype("./_data/Lato-Regular.ttf", 32, encoding='UTF-8')
 		self.counter = random.randint(1, 100)
 		img = Image.open(image)
-		font = ImageFont.load_default()
 		output = os.path.join(self.place, f"Quote: {self.counter}.jpeg")
 		x_width, x_height = img.size
 		ratio = width/ x_width
 		height = int(x_height * ratio)
 		img.thumbnail((width, height))
-
 		fill = 'white'
 		stroke_fill = 'black'
-
 		draw = ImageDraw.Draw(img)
 		rand_pos = random.choice(range(20, height-40))
 		draw.text((50, rand_pos), body, fill, font, 
                     stroke_fill=stroke_fill, stroke_width=2)
 		draw.text((50, rand_pos+30), f" - {author}", fill,
                     font=font, stroke_fill=stroke_fill, stroke_width=2)
-
 		img.save(output, 'jpeg')
 		return output
